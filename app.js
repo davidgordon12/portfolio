5e33f8c77beb5c3a7ae610951afc1cb65cb7a024
tehtko <moderndsnsam@gmail.com>
2023-02-24 20:15:59 -0500

    Blur and animations

--- app.js
+++ app.js
@@ -1,12 +1,54 @@
-// add class navbarDark on navbar scroll
+// Add class navbarDark on navbar scroll
 const header = document.querySelector('.navbar');
 
 window.onscroll = function() {
     var top = window.scrollY;
-    if(top >=100) {
+    if(top >=700) {
         header.classList.add('navbarDark');
     }
     else {
         header.classList.remove('navbarDark');
     }
 }
+
+// Blob animation
+const blob = document.getElementById("blob");
+
+window.onpointermove = event => { 
+  const { clientX, clientY } = event;
+  
+  blob.animate({
+    left: `${clientX}px`,
+    top: `${clientY}px`
+  }, { duration: 8000, fill: "forwards" });
+}
+
+// Animate text on mouse over
+const letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+
+let interval = null;
+
+document.querySelector("h2").onmouseover = event => {  
+    let iteration = 0;
+    
+    clearInterval(interval);
+    
+    interval = setInterval(() => {
+      event.target.innerText = event.target.innerText
+        .split("")
+        .map((letter, index) => {
+          if(index < iteration) {
+            return event.target.dataset.value[index];
+          }
+        
+          return letters[Math.floor(Math.random() * 26)]
+        })
+        .join("");
+      
+      if(iteration >= event.target.dataset.value.length){ 
+        clearInterval(interval);
+      }
+      
+      iteration += 1 / 3;
+    }, 30);
+  }
